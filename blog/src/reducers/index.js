ea3d0ab92371198f1cb8f633db2d6fd558538b7d
Saran <romansaran446@gmail.com>
2021-01-25 14:55:20 +0530

    reducers

---
+++ blog/src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers } from 'redux';
+import Book
+
+const rootReducer = combineReducers({
+        books: BooksReducer
+});
+
+export default rootReducer;
---
+++ blog/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+ export default function() {
+    return[
+        {title:'javascript: he Good parts'},
+        {title:'Harry potter'},
+        {title:'RIch-dad-poor-dad'},
+        {title:'Elonruby'},
+    ]
+}
